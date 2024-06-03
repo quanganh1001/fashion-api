3d576afd73e33fc8abae116019fd67ea3035dccc
quanganh <quanganhnguyen100196@gmail.com>
2024-06-03 18:48:07 +0700

    a

--- src/main/java/org/example/fashion_api/Configurations/CorsConfig.java
+++ src/main/java/org/example/fashion_api/Configurations/CorsConfig.java
@@ -5,20 +5,25 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
+
 
 @Configuration
 public class CorsConfig {
     @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
-        configuration.setAllowedMethods(List.of("*"));
-        configuration.setExposedHeaders(List.of("*"));
-        configuration.setAllowedHeaders(List.of("*"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000") // Chỉ cho phép localhost:3000
+                        .allowedMethods("*") // Cho phép tất cả phương thức HTTP
+                        .allowedHeaders("*") // Cho phép tất cả các tiêu đề
+                        .exposedHeaders("*"); // Cho phép tất cả các tiêu đề trong phản hồi
+            }
+        };
     }
 }
